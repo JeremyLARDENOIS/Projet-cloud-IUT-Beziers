a6b92f02187e628e44712622301dd1a535b65fbd
root <root@debian>
2020-01-16 14:44:10 +0100

    Installer nextcloud facilement avec un script magique

---
+++ installnextcloud.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+
+sudo apt install unzip sqlite3 apache2 php php-zip php-dom php-curl php-gd php-mbstring php-sqlite3
+phpenmod zip dom curl gd mbstring sqlite3
+wget https://download.nextcloud.com/server/releases/nextcloud-17.0.2.zip
+unzip nextcloud-17.0.2.zip -d /var/www/html
+chown -R www-data:www-data /var/www/html/nextcloud
+service apache2 restart
